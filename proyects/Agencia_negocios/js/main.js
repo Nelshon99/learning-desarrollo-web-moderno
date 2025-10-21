b6e0bcbd16a2870774c66593f63e34cdccc5f737
Nelshon99 <nelson.hernandezbadel@gmail.com>
2025-10-21 00:54:47 -0500

    Section precios,contacto y footer

--- proyects/Agencia_negocios/js/main.js
+++ proyects/Agencia_negocios/js/main.js
@@ -9,3 +9,38 @@ menu.addEventListener("click", () => {
 });
 
 // End navbar
+
+// Baging Video
+const video = document.querySelector(".video");
+const btn = document.querySelector(".buttons button i");
+const bar = document.querySelector(".video-bar");
+
+const PlayPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = "far fa-pause-circle";
+    video.style.opacity = "0.7";
+  } else {
+    video.pause();
+    btn.className = "far fa-play-circle";
+    video.style.opacity = "0.3";
+  }
+};
+
+btn.addEventListener("click", () => {
+  PlayPause();
+});
+
+video.addEventListener("timeupdate", () => {
+  console.log(video.currentTime, video.duration);
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%`;
+
+  //Final del video
+  if (video.ended) {
+    btn.className = "far fa-play-circle";
+    video.style.opacity = "0.3";
+  }
+});
+
+// End video
